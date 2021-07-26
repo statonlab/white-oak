bfdd4ce091a971fada0259f66613bda0321d5b2b
Beant Kapoor <53795346+beantkapoor786@users.noreply.github.com>
2021-07-26 10:38:22 -0400

    Delete fastq_passes_4_or_more.sh

--- fastq_passes_4_or_more.sh
+++
@@ -1,4 +0,0 @@
-# This script filters the pacbio fastq file to atleast passes = 4 in a new file called out.fastq
-# Usage: bash fastq_passes_4_or_more.sh <filename>
-
-cat $1 | sed '/passes=3/,+3d' > out.fastq
