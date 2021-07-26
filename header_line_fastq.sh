4677e258ceb058d05b570f9aaa848027ec1b84aa
Beant Kapoor <53795346+beantkapoor786@users.noreply.github.com>
2021-07-26 10:39:03 -0400

    Delete header_line_fastq.sh

--- header_line_fastq.sh
+++
@@ -1,4 +0,0 @@
-# This script takes the header line of each sequence in PacBio HiFi fastq file and prints to a new file
-# Usage bash header_line_fastq.sh <fastq_file>
-
-awk '/passes/ {print $0}' $1 >> header_line_fastq.txt
